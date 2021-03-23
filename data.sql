093abe8e2464a64ace0cb692d66ff2193fd6f7de
Notahina <52755571+Notahina@users.noreply.github.com>
2021-03-23 18:38:11 +0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -2,10 +2,29 @@ SELECT array_to_json(array_agg(row_to_json (r))) FROM ( select nom,prenom,login,
 --------------------ADD DIGEST----------------
 create extension pgcrypto;
 -----------------------UTILISATEUR-------------------------------
-INSERT INTO UTILISATEUR VALUES(nextval('user_seq'),'Rakoto','Francois','Fran56','0381569876',digest('1234','sha1'));
-INSERT INTO UTILISATEUR VALUES(nextval('user_seq'),'Rakoto','Jean','RakotoJean','0385669876', crypt('12345', gen_salt('bf', 8)),'USER');
-----------Admin------------------------
-INSERT INTO  UserAdmin values (nextval('user_seq'),'Njanah','Feliciano','Njanah','0343941152', crypt('12345', gen_salt('bf', 8)),'ADMIN');
-INSERT INTO  UserAdmin values (nextval('user_seq'),'Notahina','','Nota','0343941152', crypt('12345', gen_salt('bf', 8)),'ADMIN');
+INSERT INTO UTILISATEUR VALUES(nextval('user_seq'),'Rakoto','Jean','RakotoJean','0345669876', crypt('12345', gen_salt('bf', 8)),'USER');
+INSERT INTO UTILISATEUR VALUES(nextval('user_seq'),'Rakoto','Jean','Pierre','0347829876', crypt('12345', gen_salt('bf', 8)),'USER');
+INSERT INTO UTILISATEUR VALUES(nextval('user_seq'),'RaBe','Pierrette','Pierety','0345869876', crypt('12345', gen_salt('bf', 8)),'USER');
+INSERT INTO UTILISATEUR VALUES(nextval('user_seq'),'Rakoto','Jeannette','Jeannette','0345669176', crypt('12345', gen_salt('bf', 8)),'USER');
+INSERT INTO UTILISATEUR VALUES(nextval('user_seq'),'Rakoto','Boto','Boto','0345669816', crypt('12345', gen_salt('bf', 8)),'USER');
+
+INSERT INTO UTILISATEUR VALUES(nextval('user_seq'),'Notahina','Notahina','Nota','0349815520', crypt('12345', gen_salt('bf', 8)),'ADMIN');
+
 
 INSERT INTO MONEY_MOUVEMENT VALUES(nexvat('money_seq'),12,'depot',5000,'2021-03-07 10:11:00',0);
+	ID_MOUVEMENT int PRIMARY KEY,
+	ID_UTILISATEUR int,
+	TYPES varchar(30),
+	MONTANT double precision,
+	DATE_TRANSACTION TIMESTAMP,
+	ETAT int,
+	FOREIGN KEY (ID_UTILISATEUR) REFERENCES UTILISATEUR(ID_UTILISATEUR)
+);
+
+INSERT INTO OFFRE VALUES(2,'FETY MORA','2019-01-01 ',1,'APPEL',800);
+---------------
+INSERT INTO offre_mouvement values(1,1,1,1,1,'2021-03-09' );
+INSERT INTO offre_mouvement values(2,1,1,1,1,'2021-03-09' );
+INSERT INTO offre_mouvement values(3,2,1,1,1,'2021-03-09' );
+INSERT INTO offre_mouvement values(4,2,2,1,1,'2021-03-10' );
+INSERT INTO offre_mouvement values(5,2,2,1,1,'2021-03-10' );
